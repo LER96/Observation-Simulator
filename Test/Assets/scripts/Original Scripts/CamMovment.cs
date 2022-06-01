54e195e5d169ff5e7b654ee8f2096349a5b45ff8
LER96 <93140643+LER96@users.noreply.github.com>
2022-06-01 16:31:01 +0300

    Revert "Img camera"
    This reverts commit 9a365e2cc076e2e2d92702cbc4bb92cf959fd835.

--- Test/Assets/scripts/Original Scripts/CamMovment.cs
+++ Test/Assets/scripts/Original Scripts/CamMovment.cs
@@ -1,37 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 public class CamMovment : MonoBehaviour
 {
     float mouseX;
     float mouseY;
-    string json;
-    TargertJson save;
-
-    [Header("Objects")]
-    public Transform body;
-    public Camera cameraGameobject;
-
-    private bool isZooming;
-    [SerializeField] private float zoomSize;
-    [SerializeField] private float zoomRatio;
-    [SerializeField] private float minSpeed;
-    [SerializeField] private float maxSpeed;
-
-    [Header("Movement Speed")]
     public float sensitivityX = 5;
     public float sensitivityY = 5;
     public float inc = 25;
+    public Transform body;
+    TargertJson save;
+    string json;
+
     float xrotation;
-
-
-    [Header ("Movement TEXT")]
-    public Text side;
-    public Text top;
-
-
-
 
     // Start is called before the first frame update
     void Start()
@@ -42,8 +24,6 @@ public class CamMovment : MonoBehaviour
 
     private void Update()
     {
-        side.text = "" + sensitivityX * 10;
-        top.text = "" + sensitivityY * 10;
         if(Input.GetKeyDown(KeyCode.RightArrow) && sensitivityX<700)
         {
             sensitivityX += inc;
@@ -141,17 +121,6 @@ public class CamMovment : MonoBehaviour
             GiveTarget(4);
         }
 
-        //Zoomin/out
-        if (Input.GetKey(KeyCode.KeypadMultiply))
-        {
-            StartCoroutine(Zoom(1));
-        }
-        //If the users press / Then start zooming out
-        else if (Input.GetKey(KeyCode.KeypadDivide))
-        {
-            StartCoroutine(Zoom(-1));
-        }
-
 
     }
     void Move(int x, int y)
@@ -182,54 +151,6 @@ public class CamMovment : MonoBehaviour
         transform.localRotation = loadeRotationData.cam[num1];
         body.localRotation = loadeRotationData.mainBody[num1];
     }
-
-
-    IEnumerator Zoom(int zoom)
-    {
-        if (!isZooming)
-        {
-            //Disable the option to zoom multiple time in a row
-            isZooming = true;
-
-            //Setting the zoom itself in the camera
-            zoomSize = cameraGameobject.fieldOfView;
-
-            //Zooming in if the input was to zoom in
-            if (zoomSize <= 55 && zoom == 1)
-            {
-                //Zoom in slowly
-                for (int i = 0; i < 5; i++)
-                {
-                    zoomSize++;
-                    cameraGameobject.fieldOfView = zoomSize;
-                    yield return new WaitForSeconds(0.02f);
-                }
-                //Min Max Normlize
-                zoomRatio = (zoomSize - 5) / 55;
-                sensitivityX = minSpeed + zoomRatio * (maxSpeed - minSpeed);
-                sensitivityY = minSpeed + zoomRatio * (maxSpeed - minSpeed);
-            }
-            //Zooming out if the input was to zoom out
-            else if (zoomSize >= 6 && zoom == -1)
-            {
-                //Zoom in slowly
-                for (int i = 0; i < 5; i++)
-                {
-                    zoomSize--;
-                    cameraGameobject.fieldOfView = zoomSize;
-                    yield return new WaitForSeconds(0.02f);
-                }
-                //Min Max Normlize
-                zoomRatio = (zoomSize - 5) / 55;
-                sensitivityX = minSpeed + zoomRatio * (maxSpeed - minSpeed);
-                sensitivityY = minSpeed + zoomRatio * (maxSpeed - minSpeed);
-            }
-            //Realsing the option to zoom again
-            isZooming = false;
-        }
-
-    }
-
 
     private class TargertJson
     {
