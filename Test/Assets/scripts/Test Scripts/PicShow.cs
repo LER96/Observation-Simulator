9a365e2cc076e2e2d92702cbc4bb92cf959fd835
LER96 <93140643+LER96@users.noreply.github.com>
2022-06-01 14:40:04 +0300

    Img camera

---
+++ Test/Assets/scripts/Test Scripts/PicShow.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PicShow : MonoBehaviour
+{
+    [SerializeField] RenderTexture PhoneTexture;
+
+    public Image image;
+    private Texture2D photoCapture;
+    public Camera phoneCamera;
+    //private bool viewingPhoto = true;
+
+
+    public void Update()
+    {
+        //yield return new WaitForEndOfFrame();
+
+        var prevRenderTexture = RenderTexture.active;
+        RenderTexture.active = PhoneTexture;
+
+        photoCapture = new Texture2D(PhoneTexture.width, PhoneTexture.height);
+        photoCapture.ReadPixels(new Rect(0, 0, PhoneTexture.width, PhoneTexture.height), 0, 0);
+        photoCapture.Apply();
+
+
+        Sprite photoSprite = Sprite.Create(photoCapture,
+        new Rect(0.0f, 0.0f, photoCapture.width, photoCapture.height),
+        new Vector2(0.5f, 0.5f), 100.0f);
+        image.sprite = photoSprite;
+
+
+        RenderTexture.active = prevRenderTexture;
+        //StartCoroutine(CapturePhoto());
+
+    }
+
+    //IEnumerator CapturePhoto()
+    //{
+    //    //viewingPhoto = true;
+    //    yield return new WaitForEndOfFrame();
+
+    //    var prevRenderTexture = RenderTexture.active;
+    //    RenderTexture.active = PhoneTexture;
+
+    //    photoCapture = new Texture2D(PhoneTexture.width, PhoneTexture.height);
+    //    photoCapture.ReadPixels(new Rect(0, 0, PhoneTexture.width, PhoneTexture.height), 0, 0);
+    //    photoCapture.Apply();
+
+
+    //    Sprite photoSprite = Sprite.Create(photoCapture,
+    //    new Rect(0.0f, 0.0f, photoCapture.width, photoCapture.height),
+    //    new Vector2(0.5f, 0.5f), 100.0f);
+    //    image.sprite = photoSprite;
+
+
+    //    RenderTexture.active = prevRenderTexture;
+    //}
+
+
+}
