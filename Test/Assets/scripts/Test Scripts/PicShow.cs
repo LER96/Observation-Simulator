257b5c6418b132b5286a5a5f064343a47c8c669a
LER96 <93140643+LER96@users.noreply.github.com>
2022-06-03 23:41:20 +0300

    slight UI change

--- Test/Assets/scripts/Test Scripts/PicShow.cs
+++ Test/Assets/scripts/Test Scripts/PicShow.cs
@@ -9,8 +9,10 @@ public class PicShow : MonoBehaviour
     [SerializeField] RenderTexture PhoneTexture;
 
     public Image image;
+    public Image invetory;
     private Texture2D photoCapture;
     public Camera phoneCamera;
+    CamMovment1 press;
     //private bool viewingPhoto = true;
 
 
@@ -33,31 +35,35 @@ public class PicShow : MonoBehaviour
 
 
         RenderTexture.active = prevRenderTexture;
-        //StartCoroutine(CapturePhoto());
 
+        if(press)
+        {
+            CapturePhoto();
+        }
+        //StartCoroutine(CapturePhoto());
     }
 
-    //IEnumerator CapturePhoto()
-    //{
-    //    //viewingPhoto = true;
-    //    yield return new WaitForEndOfFrame();
+    public void CapturePhoto()
+    {
+        //viewingPhoto = true;
+        //yield return new WaitForEndOfFrame();
 
-    //    var prevRenderTexture = RenderTexture.active;
-    //    RenderTexture.active = PhoneTexture;
+        var prevRenderTexture = RenderTexture.active;
+        RenderTexture.active = PhoneTexture;
 
-    //    photoCapture = new Texture2D(PhoneTexture.width, PhoneTexture.height);
-    //    photoCapture.ReadPixels(new Rect(0, 0, PhoneTexture.width, PhoneTexture.height), 0, 0);
-    //    photoCapture.Apply();
-
-
-    //    Sprite photoSprite = Sprite.Create(photoCapture,
-    //    new Rect(0.0f, 0.0f, photoCapture.width, photoCapture.height),
-    //    new Vector2(0.5f, 0.5f), 100.0f);
-    //    image.sprite = photoSprite;
+        photoCapture = new Texture2D(PhoneTexture.width, PhoneTexture.height);
+        photoCapture.ReadPixels(new Rect(0, 0, PhoneTexture.width, PhoneTexture.height), 0, 0);
+        photoCapture.Apply();
 
 
-    //    RenderTexture.active = prevRenderTexture;
-    //}
+        Sprite photoSprite = Sprite.Create(photoCapture,
+        new Rect(0.0f, 0.0f, photoCapture.width, photoCapture.height),
+        new Vector2(0.5f, 0.5f), 100.0f);
+        invetory.sprite = photoSprite;
+
+
+        RenderTexture.active = prevRenderTexture;
+    }
 
 
 }
